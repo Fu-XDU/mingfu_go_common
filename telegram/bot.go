ef575475308e6d61275433888ebadad966215539
Fu-XDU <fuming.137@icloud.com>
2025-03-09 19:11:45 +0800

    update

--- telegram/bot.go
+++ telegram/bot.go
@@ -32,18 +32,44 @@ type SendMessageResponse struct {
 	} `json:"result"`
 }
 
-func (b *Bot) SendMessage(msg string, chatId string) (resp *SendMessageResponse, err error) {
+// SendTextMessage 发送普通文本消息
+func (b *Bot) SendTextMessage(msg string, chatId string) (resp *SendMessageResponse, err error) {
+	return b.sendMessage(msg, chatId, "")
+}
+
+// SendMarkdownMessage 发送 Markdown 格式的消息
+func (b *Bot) SendMarkdownMessage(msg string, chatId string) (resp *SendMessageResponse, err error) {
+	return b.sendMessage(msg, chatId, "Markdown")
+}
+
+// SendMarkdownV2Message 发送 MarkdownV2 格式的消息
+func (b *Bot) SendMarkdownV2Message(msg string, chatId string) (resp *SendMessageResponse, err error) {
+	return b.sendMessage(msg, chatId, "MarkdownV2")
+}
+
+// SendHtmlMessage 发送 HTML 格式的消息
+func (b *Bot) SendHtmlMessage(msg string, chatId string) (resp *SendMessageResponse, err error) {
+	return b.sendMessage(msg, chatId, "HTML")
+}
+
+// sendMessage 发送消息的底层实现
+func (b *Bot) sendMessage(msg string, chatId string, parseMode string) (resp *SendMessageResponse, err error) {
 	// 构造消息数据
 	data := map[string]interface{}{
 		"chat_id": chatId,
 		"text":    msg,
 	}
+	if len(parseMode) != 0 {
+		data["parse_mode"] = parseMode
+	}
 
+	// 发送请求
 	body, err := network.Post(b.sendMessageUrl, data)
 	if err != nil {
 		return
 	}
 
+	// 解析响应
 	resp = new(SendMessageResponse)
 	err = json.Unmarshal(body, resp)
 	if err != nil {
